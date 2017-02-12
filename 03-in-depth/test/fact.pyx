6112959d235777bdbf3426a1616fd9a4981f3b1c
Roberto Javier Homs Puron <rhoms@cells.es>
2017-02-12 11:46:04 -0800

    Time comparations

--- 03-in-depth/test/fact.pyx
+++ 03-in-depth/test/fact.pyx
@@ -3,3 +3,26 @@ def py_fact(n):
     if n<=1:
         return 1
     return n * py_fact(n - 1)
+
+def type_fact(long n):
+    """ Compute n!"""
+    if n<=1:
+        return 1
+    return n * type_fact(n - 1)
+
+
+cdef long c_fact(long n):
+    """ Compute n!"""
+    if n<=1:
+        return 1
+    return n * c_fact(n - 1)
+
+def wrap_c_fact(n):
+    """Compute n!"""
+    return c_fact(n)
+
+cpdef long cp_fact(long n):
+    """ Compute n!"""
+    if n<=1:
+        return 1
+    return n * cp_fact(n - 1)
